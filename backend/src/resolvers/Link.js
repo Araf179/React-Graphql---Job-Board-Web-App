50660e4e42151780c92a787cf81577a9d900bb88
Arafullah Shihab <shihabaraf@gmail.com>
2019-11-26 13:59:34 -0500

    Initial commit

---
+++ backend/src/resolvers/Link.js
@@ -0,0 +1,17 @@
+function postedBy(parent, args, context) {
+    return context.prisma.link({ id: parent.id }).postedBy()
+  }
+  
+  function votes(parent, args, context) {
+    return context.prisma.link({ id: parent.id }).votes()
+  }
+
+  function comments(parent, args, context) {
+    return context.prisma.link({ id: parent.id }).votes()
+  }
+  
+  module.exports = {
+    postedBy,
+    votes,
+    comments
+  }
